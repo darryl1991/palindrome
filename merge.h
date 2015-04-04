69692663889a9e5cc05c4124f64f3e3b925be0f7
darryl1991 <darryl.ramnath@gmail.com>
2015-04-03 23:46:00 -0400

    Adding file

---
+++ merge.h
@@ -0,0 +1,10 @@
+#ifndef _MERGE_H_
+#define _MERGE_H_
+
+#include <string>
+
+void mergesortstr (std::vector<std::string>& list, unsigned int low, unsigned int high);
+
+void merge(std::vector<std::string> &list, int low, int high, int mid);
+
+#endif /* _MERGE_H_ */
